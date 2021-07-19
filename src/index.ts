58aa725abbf6fe0a13730530973461934288847f
Jan Starzak <jan.starzak@gmail.com>
2021-07-19 09:00:05 +0200

    feat: export audio device and network adapter typings

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ export * from './Console'
 export * from './DebugConsole'
 export * from './VirtualMachine'
 export * from './QBasic'
+export * from './IAudioDevice'
+export * from './AudioDevice'
+export * from './INetworkAdapter'
+export * from './NetworkAdapter'
 
 import { legacyAPI } from './legacy'
 legacyAPI()
